8c9629045114fcb9083e53d0096835c1317fedeb
frankkuete <kuete.frank1@gmail.com>
2024-12-29 07:11:57 +0100

    correcting some typo

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -88,13 +88,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices')
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
    
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try{
@@ -116,16 +125,6 @@ export async function updateInvoice(id: string, formData: FormData) {
   }
 
   export async function deleteInvoice(id: string) {
-
-    try{
       await sql`DELETE FROM invoices WHERE id = ${id}`;
-      revalidatePath('/dashboard/invoices');
-      return { message: 'Database Error: Failed to Delete Invoice.' };
-    }
-    catch{
-      return {
-        message: 'Database Error: Failed to Delete Invoice.',
-      }
-    }
-    
+      revalidatePath('/dashboard/invoices');    
   }
